95811090c33e06b93c1c6216867f10041da210e2
Dawid Ostrowski <avedave@gmail.com>
2014-02-23 09:32:02 +0100

    initial project setup

---
+++ src/pl/dawidostrowski/ave01rachunek/StatusActivity.java
@@ -0,0 +1,35 @@
+package pl.dawidostrowski.ave01rachunek;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class StatusActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_status);
+		
+		Button logButton = (Button)findViewById(R.id.log_button);
+		logButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent logIntent = new Intent(StatusActivity.this, LogActivity.class);
+				startActivity(logIntent);
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.status, menu);
+		return true;
+	}
+
+}
